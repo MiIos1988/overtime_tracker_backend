7ecf74c9c99f903d63153232710f97b92da62cd0
Milos1988 <vojvoda1988@gmail.com>
2024-02-11 00:15:33 +0100

    token validation

--- routes/managerRoute.ts
+++ routes/managerRoute.ts
@@ -2,7 +2,8 @@ import express from "express";
 import tokenValidation from "../validation/tokenValidation";
 const managerRoute = express.Router();
 
-managerRoute.post("/add-manager", tokenValidation, async (req, res) => {
+managerRoute.post("/add-manager", tokenValidation
+, async (req, res) => {
     console.log("work")
     res.send("ok")
 })
