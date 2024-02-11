ed93041cb70f033a3e98dca7a6b2e1c25c249a24
Milos1988 <vojvoda1988@gmail.com>
2024-02-12 00:59:31 +0100

    Decode token

--- routes/managerRoute.ts
+++ routes/managerRoute.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import tokenValidation from "../validation/tokenValidation";
 const managerRoute = express.Router();
+import { jwtDecode } from "jwt-decode";
 
-managerRoute.post("/add-manager", tokenValidation
-, async (req, res) => {
-    console.log("work")
-    res.send("ok")
-})
+managerRoute.post("/add-manager", tokenValidation, async (req, res) => {
+    console.log(req.body)
+    const token = req.body.token;
+    if(token){
+        const decodedToken: any = jwtDecode(token);
+        console.log(decodedToken);
+    }
+  res.send("ok");
+});
 
 export default managerRoute;
