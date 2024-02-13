ad41f7fe0a99cf8f25ac20877b5408f7659385ad
Milos1988 <vojvoda1988@gmail.com>
2024-02-13 01:30:57 +0100

    Create manager in mongoDb

--- routes/managerRoute.ts
+++ routes/managerRoute.ts
@@ -2,14 +2,32 @@ import express from "express";
 import tokenValidation from "../validation/tokenValidation";
 const managerRoute = express.Router();
 import { jwtDecode } from "jwt-decode";
+import ManagerModel from "../models/managersModel";
 
 managerRoute.post("/add-manager", tokenValidation, async (req, res) => {
-    console.log(req.body)
-    const token = req.body.token;
-    if(token){
-        const decodedToken: any = jwtDecode(token);
-        console.log(decodedToken);
+  const token = req.body.token;
+  if (token) {
+    const decodedToken: any = jwtDecode(token);
+    try {
+      const existingManager = await ManagerModel.findOne({
+        userId: decodedToken.sub,
+      });
+      console.log(existingManager);
+      if (!existingManager) {
+        await ManagerModel.create({
+          email: decodedToken.email,
+          userId: decodedToken.sub,
+        });
+        return res.send("Create manager");
+      } else {
+        console.log("Exist manager");
+        return res.send("Exist manager");
+      }
+    } catch (error) {
+      console.log(error);
+      return res.send("Error manager");
     }
+  }
   res.send("ok");
 });
 
