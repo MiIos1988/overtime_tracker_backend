ec6de0eb7f8821875678f5cee959b51be98b5008
Milos1988 <vojvoda1988@gmail.com>
2024-02-27 23:51:20 +0100

    Add credentials in env

--- routes/saveImageRoute.ts
+++ routes/saveImageRoute.ts
@@ -3,10 +3,32 @@ import multer from 'multer';
 const upload = multer();
 import tokenValidation from "../validation/tokenValidation";
 const saveImageRoute = express.Router();
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({
+    region: "eu-north-1", 
+    credentials: {
+      accessKeyId: process.env.ACCESS_KEY || "",
+      secretAccessKey: process.env.SECRET_ACCESS_KEY || "",
+    },
+  });
+  
 
 saveImageRoute.post("/change-image", upload.single("image"), tokenValidation, (req, res) => {
-    console.log(req.file)
-    res.send("ok")
+    try {
+        if(req.file){
+            const uploadParams = {
+                Bucket: process.env.BUCKET_NAME,
+                Key: "UNIQUE_KEY_FOR_THE_IMAGE", // Unikatan ključ za sliku na AWS S3
+                Body: req.file.buffer, // Buffer slike
+                ContentType: req.file.mimetype, // MIME tip slike
+            };
+        }
+        console.log(req.file)
+        res.send("ok")
+    } catch (error) {
+        
+    }
 })
 
 export default saveImageRoute;
