ae87685a666a19ac4a994da51cb671bc218dd9b8
Milos1988 <vojvoda1988@gmail.com>
2024-02-28 01:41:54 +0100

    Get aws link

--- routes/saveImageRoute.ts
+++ routes/saveImageRoute.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import multer from 'multer';
 const upload = multer();
+// import * as dotenv from "dotenv";
+// dotenv.config();
 import tokenValidation from "../validation/tokenValidation";
 const saveImageRoute = express.Router();
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
@@ -14,18 +16,26 @@ const s3 = new S3Client({
   });
   
 
-saveImageRoute.post("/change-image", upload.single("image"), tokenValidation, (req, res) => {
+saveImageRoute.post("/change-image", upload.single("image"), tokenValidation, async (req, res) => {
     try {
         if(req.file){
             const uploadParams = {
                 Bucket: process.env.BUCKET_NAME,
-                Key: "UNIQUE_KEY_FOR_THE_IMAGE", // Unikatan ključ za sliku na AWS S3
-                Body: req.file.buffer, // Buffer slike
-                ContentType: req.file.mimetype, // MIME tip slike
+                Key: req.body.worker,
+                Body: req.file.buffer,
+                ContentType: req.file.mimetype, 
             };
+            const putObjectCommand = new PutObjectCommand(uploadParams);
+            const response = await s3.send(putObjectCommand);
+            console.log(response)
+            // console.log(uploadParams.Bucket)
+            // console.log(s3.config.region)
+            // console.log(uploadParams.Key)
+
+            const imageUrl = `https://${process.env.BUCKET_NAME}.s3.eu-north-1.amazonaws.com/${req.body.worker}`;
+            console.log(imageUrl)
+            res.send("ok")
         }
-        console.log(req.file)
-        res.send("ok")
     } catch (error) {
         
     }
