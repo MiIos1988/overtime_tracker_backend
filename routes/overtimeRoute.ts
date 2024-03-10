75e9648c0418a3817b249830de90ecf2ff0dc846
Milos1988 <vojvoda1988@gmail.com>
2024-03-10 22:41:52 +0100

    Save hours in overtime collection

--- routes/overtimeRoute.ts
+++ routes/overtimeRoute.ts
@@ -2,12 +2,37 @@ import express from "express";
 import tokenValidation from "../validation/tokenValidation";
 const overtimeRoute = express.Router();
 import { jwtDecode } from "jwt-decode";
+import ManagerModel from "../models/managersModel";
+import OvertimeHoursModel from "../models/overtimeHoursModel";
 
 overtimeRoute.post("/send-overtime-data", tokenValidation, async (req, res) => {
-    console.log(req.body);
-    res.send("ok")
-  })
+  const token = req.headers.authorization;
+  const { worker, hours, date } = req.body;
+  try {
+    if (token) {
+      const decodedToken: any = jwtDecode(token);
+      const manager = await ManagerModel.findOne({ userId: decodedToken.sub });
+      if (manager) {
+        console.log(manager)
+        const existWorker = manager.workers.find(
+          (wor) => wor.nameWorker === worker
+        );
+        const workerId = existWorker?._id;
+        const newOvertimeHoursData = {
+          date: new Date(date),
+          hours: hours,
+          worker: workerId
+        };
+        const newOvertimeHours = new OvertimeHoursModel(newOvertimeHoursData);
+        newOvertimeHours.save();
 
+        res.send("ok");
+      }
+    }
 
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-export default overtimeRoute
+export default overtimeRoute;
