dcef7f0be48a1fd90ac30a107a2a2050d04ac768
Milos1988 <vojvoda1988@gmail.com>
2024-03-13 01:42:56 +0100

    Overtime hours

--- routes/overtimeRoute.ts
+++ routes/overtimeRoute.ts
@@ -15,21 +15,19 @@ overtimeRoute.post("/send-overtime-data", tokenValidation, async (req, res) => {
       if (manager) {
         const existWorker = manager.workers.find(
           (wor) => wor.nameWorker === worker
-          );
-          if (existWorker) {
-            const workerId = existWorker?._id;
-            const newOvertimeHoursData = {
-              date: new Date(date),
-              hours: hours,
-              worker: workerId,
-            };
-            const newOvertimeHours = new OvertimeHoursModel(newOvertimeHoursData);
-            await newOvertimeHours.save();
-            
-            existWorker.overtimeHours.push(newOvertimeHours._id);
-            await manager.save();
-            console.log(manager);
+        );
+        if (existWorker) {
+          const workerId = existWorker?._id;
+          const newOvertimeHoursData = {
+            date: new Date(date),
+            hours: hours,
+            worker: workerId,
+          };
+          const newOvertimeHours = new OvertimeHoursModel(newOvertimeHoursData);
+          await newOvertimeHours.save();
 
+          existWorker.overtimeHours.push(newOvertimeHours._id);
+          await manager.save();
           res.send("ok");
         }
       }
@@ -39,31 +37,46 @@ overtimeRoute.post("/send-overtime-data", tokenValidation, async (req, res) => {
   }
 });
 
-overtimeRoute.post("/send-overtime-review-data", tokenValidation, async (req, res) => {
-const {worker, startDate, endDate} = req.body;
-const token = req.headers.authorization;
-console.log(req.body)
-try {
-  if(token){
-    const decodedToken: any = jwtDecode(token);
-    const manager = await ManagerModel.findOne({ userId: decodedToken.sub });
-    if (manager) {
-      const existWorker = manager.workers.find(
-        (wor) => wor.nameWorker === worker
-        );
-        if (existWorker) {
-          const overtimeHours = await OvertimeHoursModel.find({ worker: existWorker._id,
-            date: { $gte: new Date(startDate), $lte: new Date(endDate) } });
-          console.log(overtimeHours);
-          res.send("ok")
-
+overtimeRoute.post(
+  "/send-overtime-review-data",
+  tokenValidation,
+  async (req, res) => {
+    const { worker, startDate, endDate } = req.body;
+    const token = req.headers.authorization;
+    const startDateUTC = new Date(startDate);
+    startDateUTC.setUTCHours(0, 0, 0, 0);
+    const endDateUTC = new Date(endDate);
+    endDateUTC.setUTCHours(23, 59, 59, 999);
+    try {
+      if (token) {
+        const decodedToken: any = jwtDecode(token);
+        const manager = await ManagerModel.findOne({
+          userId: decodedToken.sub,
+        });
+        if (manager) {
+          const existWorker = manager.workers.find(
+            (wor) => wor.nameWorker === worker
+          );
+          if (existWorker) {
+            const overtimeHours = await OvertimeHoursModel.find({
+              worker: existWorker._id,
+              date: { $gte: new Date(startDateUTC), $lte: new Date(endDateUTC) },
+            });
+            const overtimeData = overtimeHours.map((obj) => {
+              return {
+                date: obj.date,
+                hours: obj.hours,
+                worker: existWorker.nameWorker,
+              };
+            });
+            res.send({ overtimeData });
+          }
         }
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
-} catch (error) {
-  console.log(error)
-}
-
-})
+);
 
 export default overtimeRoute;
