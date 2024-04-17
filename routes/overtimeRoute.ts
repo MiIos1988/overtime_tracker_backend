94063635596b52c3db8739db565c4c7421634e1c
Milos1988 <vojvoda1988@gmail.com>
2024-04-17 23:45:12 +0200

    Corrected code

--- routes/overtimeRoute.ts
+++ routes/overtimeRoute.ts
@@ -17,7 +17,11 @@ overtimeRoute.post("/send-overtime-data", tokenValidation, async (req, res) => {
           (wor) => wor.nameWorker === worker
         );
         if (existWorker) {
-          console.log(existWorker) 
+          const showOvertimeHours = OvertimeHoursModel.find(
+            {
+              _id: { $in: existWorker.overtimeHours }
+            });
+          console.log(showOvertimeHours, "OK working") 
           const workerId = existWorker?._id;
           const newOvertimeHoursData = {
             date: new Date(date),
